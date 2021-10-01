334e91c53255550e774576002e4b17a7f6ef6850
Manjith Madhavan <Manjith.Madhavan@xerox.com>
2021-10-01 21:40:47 +0530

    #3 integtrated api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: "./app.component.html"
 })
-export class AppComponent {
-  title = 'projectx-ui';
+export class AppComponent implements OnInit {
+
+  public appInfo: any = {
+    applicationName: "",
+    version: ""
+  };
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:9090/info").subscribe((data: any) => {
+      this.appInfo = data;
+      console.log(data);
+    });
+  };
+
+
 }
